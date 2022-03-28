6311fcd25f11d98edb4b723738849e6bf21a83a6
RexYu <yu.chenhang@gmail.com>
2022-03-28 14:32:13 +0800

    add simple test

---
+++ src/test/java/tw/com/rex/helloworld/HelloWorldApplicationTest.java
@@ -0,0 +1,14 @@
+package tw.com.rex.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTest {
+
+    @Test
+    public void main() {
+
+    }
+
+}
