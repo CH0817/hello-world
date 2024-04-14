9a077ea806e134c6004ddb26a75f9564063bf09a
rex <yu.chenhang@gmail.com>
2024-04-14 23:55:37 +0800

    Initial commit

---
+++ src/main/java/tw/com/rex/helloworld/web/controller/HelloController.java
@@ -0,0 +1,16 @@
+package tw.com.rex.helloworld.web.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+}
